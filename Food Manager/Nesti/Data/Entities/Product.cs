b8be78fd66290d3ed4749d9230c18c8b1aa53f9a
apetriceanu <apetriceanu@live.com>
2019-02-02 18:40:31 +0000

    Add migration II

---
+++ Food Manager/Nesti/Data/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Nesti.Data.Entities
+{
+    public class Product
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+        public int Barcode { get; set; }
+        public double Price { get; set; }
+
+    }
+}
---
+++ Food Manager/Nesti/Data/NsContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+using Nesti.Data.Entities;
+
+namespace Nesti.Data
+{
+    public class NsContext : DbContext {
+     
+        public NsContext(DbContextOptions<NsContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
